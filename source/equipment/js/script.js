07f99de760c5471a1421932d5ef015d38a48081a
IJou <ijou@IJou-2.local>
2018-11-05 21:20:44 +0800

    record

--- source/equipment/js/script.js
+++ source/equipment/js/script.js
@@ -269,4 +269,104 @@ $(document).ready(function () {
         })
     }
     eventBind();
+
+    // Get Personal Item Borrowing Record 
+    function getData() {
+        $.ajax({
+            url: `https://xn--pss23c41retm.tw/api/item/reservation`,
+            type: "GET",
+            success: function (result) {
+                console.log(result);
+                render(result);
+            },
+            error: function (error) {
+                console.log("error:", error);
+                $(`#itemrecord`).html(`<h4 class="red-text text-darken-2">伺服器發生錯誤，請稍後再試</h4>`);
+            }
+        });
+    }
+    getData();
+
+    function render(result) {
+        var str = "";
+        for (const key in result) {
+            const element = result[key];
+            console.log(element); // 441
+            for (const inner_key in element) {
+                const inner_element = element[inner_key];
+                console.log(inner_element);
+                for (const final_key in inner_element) {
+                    const final_element = inner_element[final_key];
+                    for (const last_key in final_element) {
+                        var last_element = final_element[last_key];
+                        if (last_element.studentID == getCookie("key")) {
+                            var start = last_element.start.replace(final_key, "").replace("T", "").replace(".000Z", "");
+                            var end = last_element.end.replace(final_key, "").replace("T", "").replace(".000Z", "");
+                            str += `<tr>
+                            <td>${last_element.itemID}</td>
+                            <td>${final_key}</td>
+                            <td id="${last_key}StartTime">${start}</td>
+                            <td id="${last_key}EndTime">${end}</td>
+                            <td id="${last_key}Title">${last_element.title}</td>
+                            <td id="${last_key}Repeat">${last_element.repeat_type}</td>
+                            <td id="${last_key}State">${last_element.state}</td>
+                            </tr>`;
+                        }
+                    }
+                }
+            }
+        }
+        $(`#itemrecord-content`).html(str);
+    }
+
+    // Get Personal Class Reservation Record 
+    function getClassData() {
+        $.ajax({
+            url: `https://xn--pss23c41retm.tw/api/reservation`,
+            type: "GET",
+            success: function (result) {
+                console.log(result);
+                classRender(result);
+            },
+            error: function (error) {
+                console.log("error:", error);
+                $(`#classroom-content`).html(`<h4 class="red-text text-darken-2">伺服器發生錯誤，請稍後再試</h4>`);
+            }
+        });
+    }
+    getClassData();
+
+    function classRender(result) {
+        var str = "";
+        for (const key in result) {
+            const element = result[key];
+            console.log(element); // 441
+            for (const inner_key in element) {
+                const inner_element = element[inner_key];
+                console.log(inner_key);
+                console.log(inner_element);
+                for (const final_key in inner_element) {
+                    const final_element = inner_element[final_key];
+                    for (const last_key in final_element) {
+                        var last_element = final_element[last_key];
+                        if (last_element.name == getCookie("key")) {
+                            var start = last_element.start.replace(final_key, "").replace("T", "").replace(".000Z", "");
+                            var end = last_element.end.replace(final_key, "").replace("T", "").replace(".000Z", "");
+                            console.log(inner_element);
+                            str += `<tr>
+                            <td>${key}</td>
+                            <td>${inner_key}</td>
+                            <td>${final_key}</td>
+                            <td id="${last_key}StartTime">${start}</td>
+                            <td id="${last_key}EndTime">${end}</td>
+                            <td id="${last_key}Title">${last_element.title}</td>
+                            <td id="${last_key}State">${last_element.state}</td>
+                            </tr>`;
+                        }
+                    }
+                }
+            }
+        }
+        $(`#classroom-content`).html(str);
+    }
 });
