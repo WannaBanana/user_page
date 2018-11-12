be4f238c711a52a5acfb4350839aa9ef67549aa5
hbdoy <felicity860128@gmail.com>
2018-11-12 22:10:15 +0800

    Update 2018-11-12T14:10:11.059Z

--- equipment/js/script.js
+++ equipment/js/script.js
@@ -299,7 +299,8 @@ $(document).ready(function () {
             type: "GET",
             success: function (data) {
                 var alertNum = 0;
-                var str = "";
+                var strBtn = "";
+                var strModal = "";
                 for (const key in data) {
                     if (data.hasOwnProperty(key)) {
                         const element = data[key];
@@ -315,30 +316,33 @@ $(document).ready(function () {
                                                     for (const last_key in final_element) {
                                                         if (final_element.hasOwnProperty(last_key)) {
                                                             alertNum++;
-                                                            str += `
+                                                            strBtn += `
                                                             <div class="box3 sb14 modal-trigger" href="#${last_key}">
                                                             <span class="boxcontent">${final_element[last_key].event}</span>
                                                             </div>
+                                                            `;
+                                                            strModal += `
                                                             <div id="${last_key}" class="modal alertmodal">
                                                             <div class="modal-content black-text">
-                                                            <h5>${final_element[last_key].event}</h5>
+                                                            <div class="eventTitle0">${final_element[last_key].event}</div>
                                                             <hr>
-                                                            <span class="eventTitle">Description</span>
+                                                            <div class="eventTitle">Description</div>
                                                             <div class="eventText">${final_element[last_key].describe}</div>
                                                             <div class="divider"></div>
-                                                            <span class="eventTitle">Location</span>
+                                                            <div class="eventTitle">Location</div>
                                                             <div class="eventText">${final_element[last_key].source}</div>
                                                             <div class="divider"></div>
-                                                            <span class="eventTitle">Time</span>
+                                                            <div class="eventTitle">Time</div>
                                                             <div class="eventText">
                                                             ${new Date(final_element[last_key].time).toLocaleDateString() + " " + new Date(final_element[last_key].time).toLocaleTimeString()}
                                                             </div>
                                                             <div class="divider"></div>
-                                                            <span class="eventTitle">State</span>
+                                                            <div class="eventTitle">State</div>
                                                             <div class="eventText">${final_element[last_key].state}</div>
                                                             </div>
                                                             </div>
                                                             `;
+                                                            console.log(final_element[last_key]);
                                                         }
                                                     }
                                                 }
@@ -351,10 +355,14 @@ $(document).ready(function () {
                     }
                 }
                 if (alertNum > 0) {
+                    M.toast({
+                        html: `You have ${alertNum} messages!`
+                    });
                     $("#alertImg").html(`
                     <span class="new badge" style="margin-left:10px;background-color:#90a4ae;">${alertNum}</span>
                     `);
-                    $("#alertContent").html(str);
+                    $("#alertContent").html(strBtn);
+                    $("#alertModal").html(strModal);
                     $('.modal').modal();
                 }
             },
@@ -364,23 +372,6 @@ $(document).ready(function () {
         })
     }
     getAlert();
-
-    // Get Personal Item Borrowing Record 
-    function getData() {
-        $.ajax({
-            url: `https://xn--pss23c41retm.tw/api/item/reservation`,
-            type: "GET",
-            success: function (result) {
-                console.log(result);
-                render(result);
-            },
-            error: function (error) {
-                console.log("error:", error);
-                $(`#itemrecord`).html(`<h4 class="red-text text-darken-2">伺服器發生錯誤，請稍後再試</h4>`);
-            }
-        });
-    }
-    getData();
 
     function render(result) {
         var str = "";
